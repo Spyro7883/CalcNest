a97051d31e7744755ecad5a1c5598952fc44cefb
Spyro7883 <spinu11_2003@yahoo.ro>
2024-02-22 23:59:47 +0200

    project finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   const basicArray = ["+", "-", "*", "/", "%"];
-  const intermediateArray = ["^", "log", "lg", "ln", "exp", "mod", "factorial"];
-  const complexArray = ["sin", "cos", "tan", "ctg"];
   const partialArray = ["1/x", ".", "neg", "="];
   const [inputValue, setInputValue] = useState('');
   const [memo, setMemo] = useState("");
   const [showInput, setShowInput] = useState(false);
   const [baseOperation, setBaseOperation] = useState("");
-  const regex = /[0-9]/gi;
+  const [history, setHistory] = useState("");
+  const [stateOperation, setStateOperation] = useState(false);
+  const regex = /^\d*\.?\d*/gi;
+
+  const prevInputValueRef = useRef(inputValue);
 
   const operations = {
     '+': (a, b) => a + b,
@@ -22,12 +24,13 @@ function App() {
   }
 
   const calculate = (state) => {
-    // console.log(`Number value is: ${String(inputValue) + state}`);
-    // let useVar = showInput ? memo : inputValue;
-
+    // let compoundVar = showInput ? memo : inputValue;
+    // if (baseOperation === "") {
+    // console.log(`Values: ${Number(String(compoundVar) + String(state))} + ${state} ${compoundVar} + ${state}`)
+    // showInput ? setMemo(Number(String(compoundVar) + String(state))) : setInputValue(Number(String(compoundVar) + String(state)));
+    // }
+    // else
     showInput ? setMemo(state) : setInputValue(state);
-    // showInput ? setMemo(Number(String(useVar) + state)) : setInputValue(Number(String(useVar) + state));
-    // showInput ? setMemo(Number(String(memo) + state)) : setInputValue(Number(String(inputValue) + state));
   };
 
   const basicOperation = (state) => {
@@ -36,27 +39,35 @@ function App() {
         setBaseOperation("+")
         setMemo(inputValue)
         setShowInput(true);
-        // setInputValue('');
+        setStateOperation(false);
         console.log("sum");
         break;
       case "-":
         setBaseOperation("-")
         setMemo(inputValue)
+        setShowInput(true);
+        setStateOperation(false)
         console.log("substraction");
         break;
       case "*":
         setBaseOperation("*")
         setMemo(inputValue)
+        setShowInput(true);
+        setStateOperation(false)
         console.log("multiplication");
         break;
       case "/":
         setBaseOperation("/")
         setMemo(inputValue)
+        setShowInput(true);
+        setStateOperation(false)
         console.log("divide");
         break;
       case "%":
         setBaseOperation("%")
         setMemo(inputValue)
+        setShowInput(true);
+        setStateOperation(false)
         console.log("divide with rest");
         break;
       default:
@@ -68,17 +79,26 @@ function App() {
     let newVar;
     switch (state) {
       case "1/x":
+        newVar = showInput ? memo : inputValue;
         console.log("inverse");
+        setInputValue(1 / newVar);
+        setShowInput(false);
         break;
       case ".":
+        newVar = showInput ? memo : inputValue;
         console.log("digit");
+        setInputValue((-1) * newVar);
+        setShowInput(false);
         break;
       case "neg":
+        newVar = showInput ? memo : inputValue;
         console.log("negation");
+        setInputValue((-1) * newVar);
+        setShowInput(false);
         break;
       case "=":
-        // setMemo(inputValue)
-        newVar = baseOperation ? operations[baseOperation](Number(memo), Number(inputValue)) : newVar;
+        setStateOperation(true);
+        newVar = baseOperation ? operations[baseOperation](Number(inputValue), Number(memo)) : newVar;
         console.log("equal");
         setInputValue(newVar);
         setShowInput(false);
@@ -95,9 +115,20 @@ function App() {
     showInput ? setMemo(newInputValue) : setInputValue(newInputValue);
   };
 
-  const handleMemo = () => {
-
+  const clearInput = () => {
+    setHistory("");
   }
+
+  useEffect(() => {
+    const insertValue = inputValue;
+    const memoValue = memo;
+    if (baseOperation !== "")
+      setHistory(`${insertValue} ${baseOperation}`);
+    if (stateOperation) {
+      setHistory(`${prevInputValueRef.current} ${baseOperation} ${memoValue} =`);
+    }
+    prevInputValueRef.current = inputValue
+  }, [inputValue, baseOperation, memo, stateOperation]);
 
   return (
     <div className="App">
@@ -107,29 +138,21 @@ function App() {
       <main>
         <section>
           <div>
-            <input type="text" onChange={handleMemo} />
+            <input type="text" value={history} />
           </div>
           <div>
             <input type="text" onChange={handleInputChange} value={showInput ? memo : inputValue} />
           </div>
+
           <div>
             {numberArray.map((number) => (
               <button key={number} onClick={() => calculate(number)}>{number}</button>
             ))}
           </div>
+          <button onClick={() => clearInput()}>Remove entry</button>
           <div>
             {basicArray.map((base_op) => (
               <button key={base_op} onClick={() => basicOperation(base_op)}>{base_op}</button>
-            ))}
-          </div>
-          <div>
-            {intermediateArray.map((inter_op) => (
-              <button key={inter_op} onClick={() => calculate(inter_op)}>{inter_op}</button>
-            ))}
-          </div>
-          <div>
-            {complexArray.map((complex_op) => (
-              <button key={complex_op} onClick={() => calculate(complex_op)}>{complex_op}</button>
             ))}
           </div>
           <div>
